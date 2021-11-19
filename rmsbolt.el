06cd3e273025b9032a12d500d885a16e743ae74f
bootstrap-prime <bootstrap.prime@gmail.com>
2021-11-19 11:03:46 -0500

    parse file directory

--- rmsbolt.el
+++ rmsbolt.el
@@ -498,7 +498,7 @@ Use SRC-BUFFER as buffer for local variables."
    (let* ((asm-format (buffer-local-value 'rmsbolt-asm-format src-buffer))
           (disass (buffer-local-value 'rmsbolt-disassemble src-buffer))
           (cmd (buffer-local-value 'rmsbolt-command src-buffer))
-          (cmd :in (file-name-directory src-buffer) (mapconcat #'identity
+          (cmd :in #p(file-name-directory src-buffer) (mapconcat #'identity
                           (list cmd
                                 "--"
                                 "-g"
